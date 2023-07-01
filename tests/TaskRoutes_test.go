c01e94ca4b75469f6544f0a904fb20fd97df31fa
eduardor2m <deveduardomelo@gmail.com>
2023-07-01 02:01:14 -0300

    feat: update

--- tests/TaskRoutes_test.go
+++ tests/TaskRoutes_test.go
@@ -1,11 +1,40 @@
 package tests
 
 import (
+	"io"
+	"net/http"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExample(t *testing.T) {
-	assert.Equal(t, 1, 1, "they should be equal")
+func TestWhenListAllTasks(t *testing.T) {
+	requestUrl := "http://localhost:9090/api/task"
+	clientRequest, err := http.NewRequest(http.MethodGet, requestUrl, nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	token := ""
+
+	clientRequest.Header.Set(echo.HeaderAuthorization, "Bearer "+token)
+
+	serverResponse, err := http.DefaultClient.Do(clientRequest)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	serverData, err := io.ReadAll(serverResponse.Body)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(string(serverData))
+
+	assert.Equal(t, http.StatusOK, serverResponse.StatusCode)
+
 }
